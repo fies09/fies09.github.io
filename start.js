f4380198356081764ee0917faae077d441dd5da3
fies09 <fanyong109@163.com>
2022-03-31 12:11:46 +0800

    Site updated: 2022-03-31 12:11:46

--- start.js
+++
@@ -1,7 +0,0 @@
-var process = require('child_process');
-
-process.exec(' hexo g -d', function (error, stdout, stderr) {
-    if (error !== null) {
-      console.log('exec error: ' + error);
-    }
-});
